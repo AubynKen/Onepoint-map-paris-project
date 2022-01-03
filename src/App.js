a0ac0badac29586f5384f3100bc034d03605cfe5
Pinglei He <pingleihe@outlook.com>
2022-01-03 19:57:49 +0100

    Commit for trying production build on heroku

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
-const App = () => {
-  
-}
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
+import TemperatureScreen from "./screens/TemperatureScreen";
+
+const App = () => (
+  <Router>
+    <Header />
+    <Container style={{ minHeight: "80vh" }}>
+      <Routes>
+        <Route path="/">
+          <Route path="temp-map" element={<TemperatureScreen />} />
+        </Route>
+      </Routes>
+    </Container>
+    <Footer />
+  </Router>
+);
 export default App;
---
+++ src/screens/TemperatureScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TemperatureMapScreen = () => {
+  return <div>Hello Temperature</div>;
+};
+
+export default TemperatureMapScreen;
