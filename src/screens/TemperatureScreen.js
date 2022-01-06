afafe7a26cf2a1cb721d4f5fbccc8de2f6efe975
Pinglei He <pingleihe@outlook.com>
2022-01-06 11:00:48 +0100

    modified temperature screen

--- src/screens/TemperatureScreen.js
+++ src/screens/TemperatureScreen.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const TemperatureMapScreen = () => {
-  return <div>Hello Temperature</div>;
+  return (
+    <div>
+      <iframe
+        title="tree-map"
+        width="500"
+        height="400"
+        frameborder="0"
+        scrolling="no"
+        allowfullscreen
+        src="https://arcg.is/0jO0OS"
+      ></iframe>
+    </div>
+  );
 };
 
 export default TemperatureMapScreen;
