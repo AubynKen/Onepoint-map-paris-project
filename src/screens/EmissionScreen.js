f52e8e612dfbbce0ee4f92cb0a82df49bf72c46d
Pinglei He <pingleihe@outlook.com>
2022-01-13 11:13:45 +0100

    tsneri

--- src/screens/EmissionScreen.js
+++ src/screens/EmissionScreen.js
@@ -4,12 +4,12 @@ const EmissionScreen = () => {
   return (
     <div>
       <iframe
-        title="tree-map"
+        title="emission-map"
         style={{ width: "100%", height: "80vh" }}
         frameBorder="0"
         scrolling="no"
         allowFullScreen
-        src="//learngis2.maps.arcgis.com/apps/Embed/index.html?webmap=44abf032f31d48be8ce2b8c4748a384d&extent=2.1724,48.8008,2.5511,48.9149&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&legendlayers=true&disable_scroll=true&theme=light"
+        src="https://learngis2.maps.arcgis.com/apps/Embed/index.html?webmap=44abf032f31d48be8ce2b8c4748a384d&extent=2.1769,48.7986,2.5556,48.914&zoom=true&previewImage=false&scale=true&details=true&legendlayers=true&active_panel=details&basemap_gallery=true&disable_scroll=true&theme=light"
       ></iframe>
     </div>
   );
