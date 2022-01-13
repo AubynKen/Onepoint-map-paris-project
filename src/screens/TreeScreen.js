ff578aedd194b2905879399985223a650d4b34b0
Pinglei He <pingleihe@outlook.com>
2022-01-13 11:16:28 +0100

    testing deployment

--- src/screens/TreeScreen.js
+++ src/screens/TreeScreen.js
@@ -11,6 +11,14 @@ const TreeScreen = () => {
         allowFullScreen
         src="https://arcg.is/0jO0OS"
       ></iframe>
+      <iframe
+        title="emission-map"
+        style={{ width: "100%", height: "80vh" }}
+        frameBorder="0"
+        scrolling="no"
+        allowFullScreen
+        src="https://learngis2.maps.arcgis.com/apps/Embed/index.html?webmap=44abf032f31d48be8ce2b8c4748a384d&extent=2.1769,48.7986,2.5556,48.914&zoom=true&previewImage=false&scale=true&details=true&legendlayers=true&active_panel=details&basemap_gallery=true&disable_scroll=true&theme=light"
+      ></iframe>
     </div>
   );
 };
